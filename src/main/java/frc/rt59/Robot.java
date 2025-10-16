c2b9018c1feb56b05b57ae25a1fdf73d9083d940
Liev Dorfman <liev@dorfman.net>
2025-10-16 07:49:21 -0400

    add led code + match time publisher

--- src/main/java/frc/rt59/Robot.java
+++ src/main/java/frc/rt59/Robot.java
@@ -4,10 +4,13 @@
 
 package frc.rt59;
 
+import java.sql.Driver;
+
 import org.littletonrobotics.junction.LoggedRobot;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -91,6 +94,7 @@ public class Robot extends LoggedRobot {
     // robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    SmartDashboard.putNumber("Match Time", DriverStation.getMatchTime());
   }
 
   /**
--- src/main/java/frc/rt59/subsystems/CandleSubsystem.java
+++ src/main/java/frc/rt59/subsystems/CandleSubsystem.java
@@ -1,30 +1,24 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.rt59.subsystems;
 
 
+import com.ctre.phoenix.led.Animation;
 import com.ctre.phoenix.led.CANdle;
 import com.ctre.phoenix.led.CANdleConfiguration;
 import com.ctre.phoenix.led.ColorFlowAnimation;
 import com.ctre.phoenix.led.RainbowAnimation;
 import com.ctre.phoenix.led.SingleFadeAnimation;
 import com.ctre.phoenix.led.ColorFlowAnimation.Direction;
-import com.ctre.phoenix6.signals.RGBWColor;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class CandleSubsystem extends SubsystemBase {
-    /** Creates a new CandleSubsystem. */
     public CANdle robotCANdle = new CANdle(59);
 
     // Starting
     ColorFlowAnimation starting = new ColorFlowAnimation(100, 50, 0, 0, 2, 8, Direction.Forward, 0);
     SingleFadeAnimation noAuto = new SingleFadeAnimation(100, 0, 0, 660, 7, 8, 0);
     SingleFadeAnimation ready = new SingleFadeAnimation(0, 0, 100, 0, 240, 8, 0);
-    RGBWColor inMatch = new RGBWColor(0, 0, 100, 0);
 
     public CandleSubsystem() {
         CANdleConfiguration robotCANdleConfig = new CANdleConfiguration();
@@ -35,16 +29,21 @@ public class CandleSubsystem extends SubsystemBase {
 
     @Override
     public void periodic() { // TODO: ADD AUTO SELECTOR
-        // This method will be called once per scheduler run
         if (DriverStation.isTeleop() && DriverStation.getMatchTime() <= 20) {
-            robotCANdle.animate(new RainbowAnimation());
+            setAnimation(new RainbowAnimation());
         } else if (DriverStation.isEnabled()) {
-            robotCANdle.setLEDs(0, 0, 100);
+            solidColor(0,0,100);
         } else if (DriverStation.isDSAttached()) {
-            robotCANdle.animate(ready);
+            setAnimation(ready);
         } else if (!DriverStation.isDSAttached()) {
-            robotCANdle.animate(new ColorFlowAnimation(100, 50, 0, 0, 2, 7, Direction.Forward, 0));
+            setAnimation(starting);
         }
 
+    }
+    public void setAnimation(Animation animation) {
+        robotCANdle.animate(animation, 0);
+    }
+    public void solidColor(int r,int g, int b) {
+        robotCANdle.setLEDs(r,g,b);
     }
 }
